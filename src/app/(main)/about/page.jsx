51ea637d76bc678b572f9f2e104f8fe20d41ce2b
Halil Akgun <halilakgun0@gmail.com>
2024-10-15 23:57:04 +0200

    day04

--- src/app/(main)/about/page.jsx
+++ src/app/(main)/about/page.jsx
@@ -1,9 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
 
 const AboutPage = () => {
-  throw new Error('Something went wrong!')
+  // throw new Error('Something went wrong!')
   return (
-    <div>AboutPage</div>
+    <div>
+      AboutPage
+
+      <Image src="https://picsum.photos/200/300" alt="img" width={200} height={300} />
+    </div>
   )
 }
 
--- src/app/(main)/contact/page.jsx
+++ src/app/(main)/contact/page.jsx
@@ -1,10 +1,19 @@
 import { wait } from '@/utils/misc'
+import Image from 'next/image';
 import React from 'react'
 
 const ContactPage = async () => {
   await wait(2000);
   return (
-    <div>ContactPage</div>
+    <div>
+      ContactPage
+      <div style={{ height: '500px', width: '500px', position: 'relative' }}>
+        <Image src="https://picsum.photos/900/700" alt="img"
+          fill // Using fill ensures the image automatically scales to fit the container's dimensions without needing explicit width and height
+          style={{ objectFit: 'cover', objectPosition: 'center' }} // objectFit ensures the image maintains its aspect ratio while covering the entire container, and objectPosition centers the image within the space
+        />
+      </div>
+    </div>
   )
 }
 
