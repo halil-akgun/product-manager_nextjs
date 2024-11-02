8a3ae4ec0e82d3a42bc280adafd92154b065e36c
Halil Akgun <halilakgun0@gmail.com>
2024-11-02 23:50:12 +0100

    day08

--- src/actions/product-actions.js
+++ src/actions/product-actions.js
@@ -1,5 +1,27 @@
 "use server"
 
+import { config } from "@/utils/config";
+import { revalidatePath } from "next/cache";
+
 export const createProductAction = async (formData) => {
-    console.log(formData);
+
+    // convert form data to object
+    const fields = Object.fromEntries(formData.entries());
+
+    try {
+        const resp = await fetch(`${config.apiURL}/products`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(fields)
+        })
+
+        if (!resp.ok) throw new Error("Failed to create product");
+
+    } catch (error) {
+
+    }
+
+    revalidatePath("/products");
 }
