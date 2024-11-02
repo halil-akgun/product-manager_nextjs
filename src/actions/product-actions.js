386f2f24a77c9e97aaa7d835960550b2e914bbdf
Halil Akgun <halilakgun0@gmail.com>
2024-11-03 00:24:59 +0100

    day08

--- src/actions/product-actions.js
+++ src/actions/product-actions.js
@@ -2,6 +2,17 @@
 
 import { config } from "@/utils/config";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import * as Yup from "yup";
+
+
+const FormSchema = Yup.object({
+    title: Yup.string().required("Title is required"),
+    description: Yup.string().required("Description is required"),
+    category: Yup.string().required("Category is required"),
+    price: Yup.number().typeError("Price must be a number").required("Price is required"),
+    image: Yup.mixed().required("Image is required"),
+})
 
 export const createProductAction = async (formData) => {
 
@@ -9,6 +20,9 @@ export const createProductAction = async (formData) => {
     const fields = Object.fromEntries(formData.entries());
 
     try {
+
+        FormSchema.validateSync(fields, { abortEarly: false });
+
         const resp = await fetch(`${config.apiURL}/products`, {
             method: "POST",
             headers: {
@@ -20,8 +34,13 @@ export const createProductAction = async (formData) => {
         if (!resp.ok) throw new Error("Failed to create product");
 
     } catch (error) {
-
+        if (error instanceof Yup.ValidationError) {
+            console.error(error)
+        }
     }
 
     revalidatePath("/products");
+    revalidatePath("/dashboard/products");
+
+    redirect("/dashboard/products");
 }
