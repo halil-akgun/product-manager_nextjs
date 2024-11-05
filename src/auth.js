0a28ae48a5c2cd35324a358bcbbcd6ae8cdfe265
Halil Akgun <halilakgun0@gmail.com>
2024-11-05 23:28:13 +0100

    day10

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,32 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 const config = {
     providers: [
-        GitHub
+        Credentials({
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+
+            async authorize(credentials) {
+                const resp = await fetch('https://dummyjson.com/auth/login', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(credentials),
+                })
+                if (!resp.ok) {
+                    return null
+                }
+
+                const user = await resp.json();
+                return user;
+            }
+        }),
+        GitHub,
+        Google
     ],
 
     callbacks: {
