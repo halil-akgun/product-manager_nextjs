1ae82dedd46f78c92b793ec6ba5b185c41be5296
Halil Akgun <halilakgun0@gmail.com>
2024-11-10 23:49:03 +0100

    edit

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,7 @@ export { auth as middleware } from "@/auth";
 
 // Eger istenirse config tanimlanarak bu kapsama alani belirlenebilir
 export const config = {
-	//matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+	//matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 
     matcher: ['/dashboard/:path*', '/login']
 };
